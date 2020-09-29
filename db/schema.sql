6dffecb5bb6eb73219c53ec4c16492163ec96058
Elliott Jones <jones.elliott@outlook.com>
2020-09-29 11:25:28 -0700

    Created Schema.sql to initialize burgers_db

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE `burgers_db`.`burgers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `burger_name` VARCHAR(45) NOT NULL,
+  `devoured` TINYINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
