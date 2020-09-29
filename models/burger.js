6ce858a56ac3875b9741adf28cac6d271d84eed0
Elliott Jones <jones.elliott@outlook.com>
2020-09-29 16:14:59 -0700

    Added Create function in burger.js to create burger in db

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,22 @@
 var orm = require('../config/orm');
 
 var burger = {
+    // Search all from burgers table
     all: function(response) {
         orm.selectAll("burgers", function(result) {
             response(result);
         });
     },
 
-    create: function(vale, response) {
-        
-    }
+    // Create burger in burgers table
+    create: function(value, response) {
+        orm.insertOne("burgers", value, function(result) {
+            response(result);
+        });
+    },
+
+    
+
 }
 
 module.exports = burger;
