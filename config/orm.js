c80d01076c086af783c8ce4420aa3119c3eb7826
Elliott Jones <jones.elliott@outlook.com>
2020-09-29 12:03:36 -0700

    Set up structure for orm.js & exported/required connection.js

---
+++ config/orm.js
@@ -0,0 +1,20 @@
+var connection = require('./connection');
+
+var orm = {
+    selectAll: function() {
+
+    },
+
+    insertOne: function() {
+        
+    },
+
+    updateOne: function() {
+        
+    }
+}
+
+
+
+// Export orm modules
+module.exports = orm;
