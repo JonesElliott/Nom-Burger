4d4484c56b3590f5312f96c68b2cdea6fe2d6d08
Elliott Jones <jones.elliott@outlook.com>
2020-09-29 14:34:26 -0700

    Added & set up imports/exports for controller and burger.js, added handlebars html files

---
+++ controllers/controller.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var orm = require('../models/burger');
+
+
+
+
+
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
