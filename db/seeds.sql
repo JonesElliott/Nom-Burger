a82110916f08965c6c4e58131ccbefc788ed2641
Elliott Jones <jones.elliott@outlook.com>
2020-09-29 11:50:15 -0700

    Created seeds.sql to pre-populate database

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Tripple Whopper with Cheese", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Clog-My-Veins Behemoth Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Super Veggie Burger", FALSE);
